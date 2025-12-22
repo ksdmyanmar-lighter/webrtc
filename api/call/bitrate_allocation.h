789d6d74b7262867a1b2384bdd5ef19dc421bb38
ksdmyanmar-lighter <ksd_founder@icloud.com>
2025-12-22 20:15:36 +0630

    Update bitrate_allocation.h

--- api/call/bitrate_allocation.h
+++ api/call/bitrate_allocation.h
@@ -18,28 +18,28 @@
 // BitrateAllocationUpdate provides information to allocated streams about their
 // bitrate allocation. It originates from the BitrateAllocater class and is
 // propagated from there.
-struct BitrateAllocationUpdate {
+//struct BitrateAllocationUpdate {
   // The allocated target bitrate. Media streams should produce this amount of
   // data. (Note that this may include packet overhead depending on
   // configuration.)
-  DataRate target_bitrate = DataRate::Zero();
+  //DataRate target_bitrate = DataRate::Zero();
   // The allocated part of the estimated link capacity. This is more stable than
   // the target as it is based on the underlying link capacity estimate. This
   // should be used to change encoder configuration when the cost of change is
   // high.
-  DataRate stable_target_bitrate = DataRate::Zero();
+  //DataRate stable_target_bitrate = DataRate::Zero();
   // Predicted packet loss ratio.
-  double packet_loss_ratio = 0;
+  //double packet_loss_ratio = 0;
   // Predicted round trip time.
-  TimeDelta round_trip_time = TimeDelta::PlusInfinity();
+  //TimeDelta round_trip_time = TimeDelta::PlusInfinity();
   // `bwe_period` is deprecated, use `stable_target_bitrate` allocation instead.
-  TimeDelta bwe_period = TimeDelta::PlusInfinity();
+  //TimeDelta bwe_period = TimeDelta::PlusInfinity();
   // Congestion window pushback bitrate reduction fraction. Used in
   // VideoStreamEncoder to reduce the bitrate by the given fraction
   // by dropping frames.
-  double cwnd_reduce_ratio = 0;
+  //double cwnd_reduce_ratio = 0;
 };
 
 }  // namespace webrtc
 
-#endif  // API_CALL_BITRATE_ALLOCATION_H_
+//endif  // API_CALL_BITRATE_ALLOCATION_H_
