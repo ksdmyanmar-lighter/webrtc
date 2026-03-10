3d3ce40b80ec343bd16ea1574122e50178de4b39
ksdmyanmar-lighter <ksd_founder@icloud.com>
2026-03-10 01:08:21 -0400

    Update resource.cc

--- api/adaptation/resource.cc
+++ api/adaptation/resource.cc
@@ -7,7 +7,7 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
-
+/*
 #include "api/adaptation/resource.h"
 
 #include "rtc_base/checks.h"
@@ -30,4 +30,4 @@ Resource::Resource() {}
 
 Resource::~Resource() {}
 
-}  // namespace webrtc
+}  // namespace webrtc*/
